2c5d07ce3d9c279999b781eb8b1e2d1d7fd30805
justinfarrelldev <justinfarrellgamedev@gmail.com>
2022-06-15 17:09:13 -0400

    Arguments support added

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,5 +1,5 @@
 pub struct Argument {
-    name: String,
+    pub name: String,
     pub description: String,
 }
 
@@ -9,12 +9,24 @@ pub struct Command {
     pub args: Vec<Argument>,
 }
 
-pub fn get_commands() -> [Command; 1] {
-    let commands = [Command {
-        name: String::from("help"),
-        description: String::from("Shows all of the available commands and their arguments."),
-        args: vec![],
-    }];
+pub fn get_commands() -> [Command; 2] {
+    let commands = [
+        Command {
+            name: String::from("help"),
+            description: String::from("Shows all of the available commands and their arguments."),
+            args: vec![],
+        },
+        Command {
+            name: String::from("new"),
+            description: String::from("Creates a new todo list."),
+            args: vec![Argument {
+                name: String::from("--name"),
+                description: String::from(
+                    "The name of the list to create (will generate a file with the list name).",
+                ),
+            }],
+        },
+    ];
 
     return commands;
 }
--- src/help/mod.rs
+++ src/help/mod.rs
@@ -4,5 +4,11 @@ pub fn help() {
     println!("================ COMMANDS LIST ================");
     for command in get_commands() {
         println!("{}: {}", command.name, command.description);
+        if command.args.len() > 0 {
+            println!("\tArguments: ");
+        }
+        for arg in command.args {
+            println!("\t\t{}: {}", arg.name, arg.description);
+        }
     }
 }
